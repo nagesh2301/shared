2b8578f2ad38c2c48d1d2e32732f6b844823002e
nagesh2301 <115499779+nagesh2301@users.noreply.github.com>
2022-10-31 00:20:39 +0530

    Update shared.groovy

--- vars/shared.groovy
+++ vars/shared.groovy
@@ -8,3 +8,8 @@ def newMaven()
 {
   sh 'mvn package'
 }
+
+def newDeploy(jobname,IP, context)
+{
+  sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${172.31.21.218}:/var/lib/tomcat8/webapps/${context}.war"
+}
